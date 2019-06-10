4ba0e4b18c669ce78fa4df897912af75d1f6b86a
18300603955 <oceaniu@163.com>
2019-06-10 17:19:26 +0800

    books

---
+++ src/main/java/com/niu/springboot/controller/BookController.java
@@ -0,0 +1,34 @@
+package com.niu.springboot.controller;
+
+import com.niu.springboot.bean.Book;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ami on 2019/6/10.
+ */
+@Controller
+public class BookController {
+    @GetMapping("/books")
+    public ModelAndView books(){
+        ModelAndView mv = new ModelAndView();
+        List<Book> books = new ArrayList<>();
+        Book b1 = new Book();
+        b1.setId(1);
+        b1.setName("三国演义");
+        b1.setAuthor("罗贯中");
+        Book b2 = new Book();
+        b2.setId(2);
+        b2.setName("红楼梦");
+        b2.setAuthor("曹雪芹");
+        books.add(b1);
+        books.add(b2);
+        mv.addObject("books",books);
+        mv.setViewName("books");
+        return mv;
+    }
+}
