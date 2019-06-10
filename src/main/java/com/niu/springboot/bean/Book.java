bad1ae97ebd6ce9b7f3d3ef9ab9f88c3df69d4c6
18300603955 <oceaniu@163.com>
2019-06-10 17:38:19 +0800

    json

--- src/main/java/com/niu/springboot/bean/Book.java
+++ src/main/java/com/niu/springboot/bean/Book.java
@@ -1,5 +1,9 @@
 package com.niu.springboot.bean;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.util.Date;
+
 /**
  * Created by ami on 2019/6/10.
  */
@@ -7,6 +11,25 @@ public class Book {
     private Integer id;
     private String name;
     private String author;
+    private Float price;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date publicationDate;
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
 
     public Integer getId() {
         return id;
@@ -38,6 +61,8 @@ public class Book {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", author='" + author + '\'' +
+                ", price=" + price +
+                ", publicationDate=" + publicationDate +
                 '}';
     }
 }
--- src/main/java/com/niu/springboot/controller/BookController.java
+++ src/main/java/com/niu/springboot/controller/BookController.java
@@ -3,15 +3,18 @@ package com.niu.springboot.controller;
 import com.niu.springboot.bean.Book;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
  * Created by ami on 2019/6/10.
  */
-@Controller
+@RestController
 public class BookController {
     @GetMapping("/books")
     public ModelAndView books(){
@@ -30,5 +33,17 @@ public class BookController {
         mv.addObject("books",books);
         mv.setViewName("books");
         return mv;
+    }
+
+    @GetMapping("/book")
+    @ResponseBody
+    public Book book(){
+        Book b1 = new Book();
+        b1.setId(1);
+        b1.setName("三国演义");
+        b1.setAuthor("罗贯中");
+        b1.setPrice(39f);
+        b1.setPublicationDate(new Date());
+        return b1;
     }
 }
