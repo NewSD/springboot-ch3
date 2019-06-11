81e132b712b970c34086b8da42880aa303ba64f2
18300603955 <oceaniu@163.com>
2019-06-11 13:52:14 +0800

    static

--- src/main/java/com/niu/springboot/controller/BookController.java
+++ src/main/java/com/niu/springboot/controller/BookController.java
@@ -1,7 +1,6 @@
 package com.niu.springboot.controller;
 
 import com.niu.springboot.bean.Book;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
